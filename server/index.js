7727331a586e94386e62c9f1d4370d4f4957d4f9
Ossama Bin Bilal <ossama.bilal@devsinc.com>
2023-08-23 21:47:00 +0500

    fix: Remove error-causing code
    - Deleted lines of code that were causing an error.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const kekw = require("./routes/kekw");
 const createDatabase = require("./scripts/createDatabase");
 
 const { faker } = require("@faker-js/faker");
@@ -10,8 +9,6 @@ const { faker } = require("@faker-js/faker");
 // });
 
 app.use(express.json());
-
-app.use("/kekw", kekw);
 
 const PORT = 4000;
 app.listen(PORT, () => {
