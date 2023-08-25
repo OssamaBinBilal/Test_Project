66d90a7578ce75f403f01800dc26ace04e2f14c4
Ossama Bin Bilal <ossama.bilal@devsinc.com>
2023-08-25 14:34:39 +0500

    feat: Implement create student API
    - Added a new API route to create student entries.
    - The API handles student information and password hashing.

--- server/routes/students.js
+++ server/routes/students.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getPaginatedActiveExams } = require("../controllers/exams");
+const { createStudent } = require("../controllers/students");
 
 router.get("/exams", getPaginatedActiveExams);
+router.post("/", createStudent);
 
 module.exports = router;
